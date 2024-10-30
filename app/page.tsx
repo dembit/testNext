7c5ceaa5b28bce8a40f6bfb29ea7523f0b96242a
Dmitry <dembit1991@gmail.com>
2024-10-30 23:30:30 +0700

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 import Client from "./Client";
 
 export default async function Page() {
-  const data = await fetch("http://localhost:3000/posts");
-  const posts = await data.json();
+  // const data = await fetch("/posts");
+  // const posts = await data.json();
 
   return (
     <main className={`flex min-h-screen flex-col p-6  antialiased`}>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
-        <Client data={posts} />
+        {/* <Client data={posts} /> */}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
