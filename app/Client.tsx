5a87e46b02ae657a5aeb1a1d627acadeee093ab1
Dmitry <dembit1991@gmail.com>
2024-10-30 23:16:43 +0700

    test

---
+++ app/Client.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Client({ data }: any) {
+  console.log(data);
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <h1 className="text-6xl font-bold">Client</h1>
+    </div>
+  );
+}
---
+++ app/posts/route.tsx
@@ -0,0 +1,10 @@
+import { redirect } from "next/navigation";
+
+export const revalidate = 60;
+
+export async function GET() {
+  const data = await fetch("https://api.vercel.app/blog");
+  const posts = await data.json();
+
+  return Response.json(posts);
+}
